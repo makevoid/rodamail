90454a847086f8d276272a4f2870aeba363994a9
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-01-08 11:46:12 +0000

    load recipient.txt file

--- config/env.rb
+++ config/env.rb
@@ -4,11 +4,18 @@ require_relative "../lib/mailer"
 
 APP_ENV = ENV["RACK_ENV"] || "development"
 
+PATH = File.expand_path "../../", __FILE__
+
+recipient_file = File.expand_path "#{PATH}/config/recipient.txt"
+recipient_address = if File.exists? recipient_file
+  File.read(recipient_file).strip
+end
+
 # ------------------------------------------------------
 # Configurations:
 #
 # the receiving address(es) of all the email messages
-ADDRESS_RECIPIENT = "francesco@appliedblockchain.com"
+ADDRESS_RECIPIENT = recipient_address || "francesco@appliedblockchain.com"
 #
 # the email / gmail account that will send the message (you have to have the password to authenticate)
 SENDER_ADDRESS    = 'm4kevoid@gmail.com'
